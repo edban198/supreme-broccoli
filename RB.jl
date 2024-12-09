811dbd31a834bba1696a4c387c87b22cb74ccc2d
Edward Bancroft <edwardbancroft70@gmail.com>
2024-12-09 00:17:21 +0000

    finished set up for ncc sim

--- RB.jl
+++ RB.jl
@@ -97,7 +97,7 @@ set!(model, u=uᵢ, w=uᵢ, T=Tᵢ)
 
 # Setting up sim
 
-simulation = Simulation(model, Δt=20seconds, stop_time = 10days)
+simulation = Simulation(model, Δt=20seconds, stop_time = 20days)
 
 wizard = TimeStepWizard(cfl=1.0, max_Δt=30seconds)
 simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(100))
