a27bab006a4b49faca82b98516ec579136a5a59b
edban198 <edwardbancroft70@gmail.com>
2024-12-18 21:34:30 +0000

    still broken

--- two-dimensional-diffusion.jl
+++ two-dimensional-diffusion.jl
@@ -3,58 +3,72 @@
 #using Pkg
 #pkg"add Oceananigans, CairoMakie"
 
+using CairoMakie
+using LaTeXStrings
 using Oceananigans
+using Oceananigans.Units: meter, meters, day, days, hours, hour, minutes, minute, seconds
+using Printf
 
 #build grid
 
-grid = RectilinearGrid(size = (128,128),
-                       x = (-0.5, 0.5), y = (-0.5, 0.5),
-                       topology = (Periodic, Periodic, Flat))   #assign Flat to x and y for 1d problem
+filename = "2d_diffusion"
 
-closure = ScalarDiffusivity(ν=0.1, κ=0.1)  #use ScalarDiffusivity for either molecular or turbulent diffusion
+const Lx = 10
+const Ly = 10
+const Nx = 64
+const Ny = 64
 
-U(x, y, t) = cos(2π * y)
+grid = RectilinearGrid(CPU(); size = (Nx,Ny),
+                       x = (-Lx/2, Lx/2), y = (-Ly/2, Ly/2),
+                       topology = (Periodic, Periodic, Flat)
+)   #assign Flat to x and y for 1d problem
 
-V(x, y, t) = -cos(2π * x)
+const ν=0.1
+const κ=0.1
 
-model = NonhydrostaticModel(; grid, closure, tracers=:T, background_fields = (u=U, v=V,))    #by default NonhydrostaticModel has no flux b.c on all fields
+closure = ScalarDiffusivity(ν=ν, κ=κ)  #use ScalarDiffusivity for either molecular or turbulent diffusion
+
+const A = 2π 
+
+U(x, y, t) = A * cos(2π * y)
+
+V(x, y, t) = -A * cos(2π * x)
+
+model = NonhydrostaticModel(; grid,
+                            closure,
+                            tracers=:T,
+                            background_fields = (u=U, v=V,)
+)    #by default NonhydrostaticModel has no flux b.c on all fields
 
 #set initial condition on temp field
-width = 0.1
-initial_temperature(x, y) = exp(-(x^2+y^2) / (2width^2))
+const width = 0.5
+const A₀ = 5
+initial_temperature(x, y) = A₀ * exp(-(x^2+y^2) / (2width^2))
 set!(model, T=initial_temperature)
 
 #Visualising model data
 using CairoMakie
 set_theme!(Theme(fontsize = 24, linewidth = 3))
 
-fig = Figure()
-axis = (xlabel = "x",
-        ylabel = "y")
-label = "t=0"
-heatmap(model.tracers.T; label, axis)
-
-
-#Running a simulation - Time-scale for diffusion across a grid cell
-min_Δx = minimum_xspacing(model.grid)
-diffusion_time_scale = min_Δx^2 / model.closure.κ.T
-
 simulation = Simulation(model,
-                        Δt = 0.1 * diffusion_time_scale,
-                        stop_iteration = 20000
+                        Δt = 30seconds,
+                        stop_time = 1day
 )
 
-#sim will run for 1000 iterations with a time-step that resolves the time-scale at which our temperature field diffusion_time_scale
-#run!(simulation)
+progress_message(sim) = @printf("Iteration: %04d, time: %s, Δt: %s, max(|u|) = %.1e ms⁻¹, max(|v|) = %.1e ms⁻¹, wall time: %s\n",
+                                iteration(sim), prettytime(sim), prettytime(sim.Δt),
+                                maximum(abs, sim.model.velocities.u), maximum(abs, sim.model.velocities.v),
+                                prettytime(sim.run_wall_time)
+)
+
+add_callback!(simulation, progress_message, IterationInterval(100))
 
 #log data to .jld2 file as simulation progresses
-filename = "Diffusion\\two-dimensional-diffusion"
 simulation.output_writers[:temperature] = JLD2OutputWriter(model, model.tracers,
                                                            filename = filename * ".jld2",
-                                                           schedule = IterationInterval(100),
+                                                           schedule = IterationInterval(10minutes),
                                                            overwrite_existing = true
 )
-
 
 run!(simulation)
 
@@ -65,17 +79,15 @@ times = T_timeseries.times
 
 xT, yT = nodes(T_timeseries)
 
-using CairoMakie
 set_theme!(Theme(fontsize = 24))
 
 fig = Figure(size = (1000,800))
 
 axis_kwargs = (xlabel = "x", ylabel = "y",
-               limits = ((-0.5,0.5), (-0.5,0.5)),
+               limits = ((-Lx/2, Lx/2), (-Ly/2, Ly/2)),
                aspect = AxisAspect(1)
 )
 
-using LaTeXStrings
 
 ax_T = Axis(fig[1,1]; title = L"Two-dimensional diffusion in a background field of $\Phi = \frac{1}{2\pi} (\sin(2\pi x / L) + \sin(2\pi y / L))$", axis_kwargs...)
 
@@ -83,14 +95,17 @@ n = Observable(1)
 
 T = @lift T_timeseries[$n]
 
-heatmap!(ax_T, T; colormap = :thermometer) #, colorrange = (0,1)
+Tlims = (minimum(abs, interior(T_timeseries)), maximum(abs, interior(T_timeseries)))
 
-CairoMakie.activate!(type = "png")
-save("Diffusion\\initial-temperature.png", fig)
+hm_T = heatmap!(ax_T, T; colormap = :thermometer, colorrange = Tlims)
+Colorbar(fig[1,2], hm_T)
+
+title = @lift "t = " * prettytime(times[$n])
+Label(fig[1, 1:2], title, fontsize = 24, tellwidth=true)
 
 #record movie
 frames = 1:length(times)
 @info "Making an animation..."
-record(fig, filename * ".mp4", frames, framerate=32) do i
+record(fig, filename * ".mp4", frames, framerate=4) do i
     n[] = i
 end
