4e7fc2dafacf787d438d2589f52945173b9f7a21
Edward Bancroft <edwardbancroft70@gmail.com>
2025-01-22 16:05:48 +0000

    tanh mask

--- plot_sponge_mask.py
+++ plot_sponge_mask.py
@@ -32,7 +32,32 @@ plt.legend()
 plt.grid()
 
 # Save the plot as an image
-plt.savefig("OUTPUTS/bottom_mask_function_flipped.png", dpi=300, bbox_inches='tight')
+#plt.savefig("OUTPUTS/bottom_mask_function_1.png", dpi=300, bbox_inches='tight')
+#plt.show()
+
+#print("Plot saved as 'OUTPUTS/bottom_mask_function_1.png'")
+
+z_sponge = sponge_one
+width_sponge = 1
+def mask_2(z):
+    return - 10*(np.tanh((z+H/2)) - 1)
+
+# Generate z-values for plotting
+z_values_2 = np.linspace(-H, 0, 500)  # From -H to 0 with 500 points
+mask_values_2 = mask_2(z_values_2)
+
+# Plot the bottom mask function with flipped axes
+plt.figure(figsize=(10, 6))
+plt.plot(mask_values_2, z_values_2, label="Bottom Mask Function(tanh)")  # Flip axes
+plt.axhline(z_sponge, color='red', linestyle='--', label="z_sponge")
+plt.title("Bottom Mask Function (tanh)")
+plt.xlabel("Mask Value")
+plt.ylabel("z (meters)")
+plt.legend()
+plt.grid()
+
+# Save the plot as an image
+plt.savefig("OUTPUTS/bottom_mask_function_tanh.png", dpi=300, bbox_inches='tight')
 plt.show()
 
-print("Plot saved as 'OUTPUTS/bottom_mask_function_flipped.png'")
+print("Plot saved as 'OUTPUTS/bottom_mask_function_tanh.png'")
