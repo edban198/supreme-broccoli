aebcabab8e8c0c085e7c3801cad01227470c9d09
Edward Bancroft <edwardbancroft70@gmail.com>
2025-01-28 16:41:57 +0000

    added separate jobscript for cpu jobs

---
+++ cpu_run_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Instructing SLURM to locate and assign
+#X number of nodes with Y number of
+#cores in each node.
+# X,Y are integers. Refer to table for various combinations
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH -p cpu
+#SBATCH --qos=short
+#SBATCH -t 08:00:00
+
+#SBATCH --job-name=RB_cpu_simulation
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user sfbj55@durham.ac.uk
+
+# Run the program
+cd
+
+./julia-1.11.2/bin/julia ./CODE/supreme-broccoli/RB_with_wind_2.jl
