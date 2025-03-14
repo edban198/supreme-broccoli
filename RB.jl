ff9ab96f636fcd1679b1615abcbb77248b00e0d2
Edward Bancroft <edwardbancroft70@gmail.com>
2025-03-14 15:53:04 +0000

    upped wind

--- RB.jl
+++ RB.jl
@@ -13,8 +13,8 @@ filename = "./OUTPUTS/RB_gpu_simulation"
 
 @info"Setting up model"
 
-const Nx = 8     # number of points in each of horizontal directions
-const Nz = 8          # number of points in the vertical direction
+const Nx = 32     # number of points in each of horizontal directions
+const Nz = 32          # number of points in the vertical direction
 
 const Lx = 4     # (m) domain horizontal extents
 const Lz = 1          # (m) domain depth
@@ -42,7 +42,7 @@ t_ff_days = t_ff / (3600 * 24)
 
 T_bcs = FieldBoundaryConditions(top = ValueBoundaryCondition(0), bottom = ValueBoundaryCondition(Δ))
 
-τx = -1e-4
+τx = -1e-3
 u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(τx))
 
 closure = ScalarDiffusivity(ν=ν,κ=κ)
