a97f06022bb9b087ca84145f3091b9fe5829fcf5
Edward Bancroft <edwardbancroft70@gmail.com>
2025-03-16 18:52:56 +0000

    stab

--- RB.jl
+++ RB.jl
@@ -73,9 +73,9 @@ set!(model, u=uᵢ, w=uᵢ, T=Tᵢ)
 
 # Setting up sim
 
-simulation = Simulation(model, Δt=1seconds, stop_time=30days)
+simulation = Simulation(model, Δt=5seconds, stop_time=30days)
 
-wizard = TimeStepWizard(cfl=0.2, max_Δt=30seconds)
+wizard = TimeStepWizard(cfl=0.2, max_Δt=5seconds)
 simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(50))
 
 # Print a progress message
