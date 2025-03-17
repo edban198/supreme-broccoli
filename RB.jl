16095528f3a434f17682f8a01d80b7f5fd416717
Edward Bancroft <edwardbancroft70@gmail.com>
2025-03-17 00:52:44 +0000

    sim

--- RB.jl
+++ RB.jl
@@ -22,7 +22,7 @@ const Lz = 4          # (m) domain depth
 grid = RectilinearGrid(CPU(); size = (Nx, Nz),
                        x = (0,Lx),
                        z = (0,Lz),
-                       topology = (Bounded, Flat, Bounded)
+                       topology = (Periodic, Flat, Bounded)
 )
 
 # Buoyancy that depends on temperature:
