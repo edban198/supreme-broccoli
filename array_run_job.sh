58f0976485d4743b4564a071b45a08e4008fff44
edban198 <edwardbancroft70@gmail.com>
2025-03-27 18:29:06 +0000

    f

--- array_run_job.sh
+++ array_run_job.sh
@@ -22,4 +22,4 @@ GAMMAS=(1 1.1 1.2 1.4 2 3 4 6 10 15 20 30 40 50)
 GAMMA=${GAMMAS[$SLURM_ARRAY_TASK_ID - 1]}
 
 # Run the Julia script with this gamma
-./julia-1.11.2/bin/julia ./CODE/supreme-broccoli/RB.jl $GAMMA
+./julia-1.11.2/bin/julia ./CODE/supreme-broccoli/array_RB.jl $GAMMA
