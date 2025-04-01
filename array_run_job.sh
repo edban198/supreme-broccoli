55f45661b4d5e9ec14e05e7cbe1d02b10be06ac7
Edward Bancroft <edwardbancroft70@gmail.com>
2025-04-01 23:29:45 +0100

    sim pls

--- array_run_job.sh
+++ array_run_job.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
-#SBATCH --job-name=RB_cpu_simulation_array
+#SBATCH --job-name=RB_sim
 #SBATCH -N 1
-#SBATCH -c 16
+#SBATCH -c 4
 #SBATCH -p cpu
 #SBATCH --qos=short
-#SBATCH -t 18:00:00
-#SBATCH --array=1-266
+#SBATCH -t 1-00:00:00
+#SBATCH --array=0-186    # Adjust depending on total combinations
 #SBATCH -o RB_sim_%A_%a.out
 #SBATCH -e RB_sim_%A_%a.err
-#SBATCH --mail-type=END
-#SBATCH --mail-user=sfbj55@durham.ac.uk
 
-cd
-
-# Parameter values to simulate
-Prs=(1 1.25 1.5 1.75 2 3 4 5 6 8 10 12 15 20 30 40 60 80 100)19
-Chis=(1 1.1 1.2 1.3 1.5 1.75 2 3 4 5 7.5 10 20 50)14
+# Define arrays (NO COMMAS, NO QUOTES)
+Prs=(1 1.5 2 3 4 5 6 8 10 12 15 20 30 40 60 80 100)
+Chis=(1 1.2 1.5 2 3 4 5 7.5 10 20 50)
 
 N_Prs=${#Prs[@]}
 N_Chis=${#Chis[@]}
 
-index=$((SLURM_ARRAY_TASK_ID - 1))
+index=$SLURM_ARRAY_TASK_ID
+Pr_index=$((index % N_Prs))
+Chi_index=$((index / N_Prs))
 
-Prs_index=$(( index % N_Prs ))
-Chis_index=$(( index / N_Prs ))
+Pr_val=${Prs[$Pr_index]}
+Chi_val=${Chis[$Chi_index]}
 
-Pr_val=${Prs[$Prs_index]}
-Chi_val=${Chis[$Chis_index]}
+echo "Running: Pr = $Pr_val, chi = $Chi_val"
 
-echo "Calling Julia with Pr=$Pr_val, χ=$Chi_val"
-~/julia-1.11.2/bin/julia ./CODE/supreme-broccoli/array_RB.jl $Pr_val $Chi_val
+# Run your Julia script WITHOUT quotes
+~/julia-1.11.2/bin/julia ~/CODE/supreme-broccoli/array_RB.jl $Pr_val $Chi_val
