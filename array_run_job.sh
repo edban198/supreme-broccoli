0ba96b8fc9279749b9243ff0e41ddb4416dbd2e1
Edward Bancroft <edwardbancroft70@gmail.com>
2025-04-04 00:15:37 +0100

    final big sim of ts

--- array_run_job.sh
+++ array_run_job.sh
@@ -5,25 +5,37 @@
 #SBATCH -p cpu
 #SBATCH --qos=short
 #SBATCH -t 2-00:00:00
-#SBATCH --array=0-63%48     # Array index matches number of Pr values (or use 0-215%64 later)
+#SBATCH --array=0-809%64        # 800 jobs, up to 64 running at once
 #SBATCH -o RB_sim_%A_%a.out
 #SBATCH -e RB_sim_%A_%a.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sfbj55@durham.ac.uk
 
-# Define Prandtl numbers
-Prs=(2.0 2.127 2.254 2.381 2.508 2.635 2.762 2.889 3.016 3.143 3.27 3.397 3.524 3.651 3.778 3.905 4.032 4.159 4.286 4.413 4.54 4.667 4.794 4.921 5.048 5.175 5.302 5.429 5.556 5.683 5.81 5.937 6.064 6.19 6.317 6.444 6.571 6.698 6.825 6.952 7.079 7.206 7.333 7.46 7.587 7.714 7.841 7.968 8.095 8.222 8.349 8.476 8.603 8.73 8.857 8.984 9.111 9.238 9.365 9.492 9.619 9.746 9.873 10.0)
+Prs=(2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9
+     3.0 3.1 3.2 3.3 3.4 3.5 3.6 3.7 3.8 3.9
+     4.0 4.1 4.2 4.3 4.4 4.5 4.6 4.7 4.8 4.9
+     5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7 5.8 5.9
+     6.0 6.1 6.2 6.3 6.4 6.5 6.6 6.7 6.8 6.9
+     7.0 7.1 7.2 7.3 7.4 7.5 7.6 7.7 7.8 7.9
+     8.0 8.1 8.2 8.3 8.4 8.5 8.6 8.7 8.8 8.9
+     9.0 9.1 9.2 9.3 9.4 9.5 9.6 9.7 9.8 9.9
+     10.0)
 
-# Get index from SLURM
+Chis=(5 6 7 8 9 10 11 12 13 14)
+
+# Compute array indices
+nPr=${#Prs[@]}      # 80
+nChi=${#Chis[@]}    # 10
+
 index=$SLURM_ARRAY_TASK_ID
+iPr=$(( index % nPr ))
+iChi=$(( index / nPr ))
 
-# Extract Pr value
-Pr_val=${Prs[$index]}
+# Get parameter values
+Pr_val=${Prs[$iPr]}
+Chi_val=${Chis[$iChi]}
 
-# Fixed chi value
-Chi_val=10
+echo "Running simulation with: Pr = $Pr_val, Chi = $Chi_val"
 
-echo "Running simulation with: Pr = $Pr_val, chi = $Chi_val"
-
-# Run the Julia script
+# Run Julia script
 ~/julia-1.11.2/bin/julia ~/CODE/supreme-broccoli/array_RB.jl $Pr_val $Chi_val
