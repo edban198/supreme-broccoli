704ead03b32f45a7adcebc4be70ec7ba471ed6da
Edward Bancroft <edwardbancroft70@gmail.com>
2025-04-08 11:02:45 +0100

    paper recreate

--- array_RB.jl
+++ array_RB.jl
@@ -11,8 +11,8 @@ using Oceananigans.Units: second, seconds, minute, minutes, hour, hours, day, da
 using Base.Threads
 @info "Number of Julia threads: $(nthreads())"
 
-const χ = "pp"
-const R = parse(Float64, ARGS[2]) #1707.76 * χ
+const χ = parse(Float64, ARGS[2])
+const R =  1707.76 * χ
 const Pr = parse(Float64, ARGS[1])
 const κ = 5e-4
 const ν = Pr * κ
--- array_run_job.sh
+++ array_run_job.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 #SBATCH --job-name=RB_sim
 #SBATCH -N 1
-#SBATCH -c 4
+#SBATCH -c 8
 #SBATCH -p cpu
 #SBATCH --qos=short
 #SBATCH -t 2-00:00:00
-#SBATCH --array=1-64%16
+#SBATCH --array=1-28%8
 #SBATCH -o RB_sim_%A_%a.out
 #SBATCH -e RB_sim_%A_%a.err
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=sfbj55@durham.ac.uk
+#SBATCH --mail-user sfbj55@durham.ac.uk
 
-# Get line number based on SLURM_ARRAY_TASK_ID
-line_number=$SLURM_ARRAY_TASK_ID
+Prs=(1 7)
+CHIS=(1 1.1 1.2 1.4 2 3 4 6 10 15 20 30 40 50)
 
-# Read Pr and R from the corresponding line in the CSV (skip header)
-read R_val Pr_val <<< $(awk -F, -v line=$line_number 'NR == line + 1 { print $1, $2 }' ~/CODE/supreme-broccoli/points_in_region_I.csv)
+N_Prs=${#Prs[@]}
+N_Chis=${#Chis[@]}
 
-echo "Running simulation with: Pr = $Pr_val, R = $R_val"
+index=$SLURM_ARRAY_TASK_ID
+Pr_index=$((index % N_Prs))
+Chi_index=$((index / N_Prs))
 
-# Run Julia script (now expects Pr and R)
-~/julia-1.11.2/bin/julia ~/CODE/supreme-broccoli/array_RB.jl $Pr_val $R_val
+Pr_val=${Prs[$Pr_index]}
+Chi_val=${Chis[$Chi_index]}
+
+echo "Running: Pr = $Pr_val, chi = $Chi_val"
+
+~/julia-1.11.2/bin/julia --threads=$SLURM_CPUS_PER_TASK ~/CODE/supreme-broccoli/array_RB.jl $Pr_val $Chi_val
