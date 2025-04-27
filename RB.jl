e95c2b4abc27f81db04a9dcf8f83ff3018ffc103
Edward Bancroft <edwardbancroft70@gmail.com>
2025-04-27 12:54:40 +0100

    GPU

--- RB.jl
+++ RB.jl
@@ -25,7 +25,7 @@ const Nz = 512          # number of points in the vertical direction
 const Lx = 8     # (m) domain horizontal extents
 const Lz = 4          # (m) domain depth
 
-grid = RectilinearGrid(CPU(); size = (Nx, Nz),
+grid = RectilinearGrid(GPU(); size = (Nx, Nz),
                        x = (0,Lx),
                        z = (0,Lz),
                        topology = (Periodic, Flat, Bounded)
